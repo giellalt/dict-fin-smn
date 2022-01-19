b5c8417a4098a93d8e208df3ac7b06f9a7583520
Marja-Liisa Olthuis <marja-liisa.olthuis@oulu.fi>
2022-01-19 12:26:52 +0000

    ođđa bargu

--- scripts/c2x.pl
+++ scripts/c2x.pl
@@ -7,26 +7,51 @@ use utf8 ;
 
 print STDOUT "<r>\n";
 
+
 while (<>) 
+
+
 {
 	chomp ;
-	my ($POS, $A, $B, $C, $D, $E, $F, $G, $H, $I, $J, $K) = split /\t/ ;
-	print STDOUT "   <e src=\"MO\">\n";
+	my ($lemma, $POS, $trans) = split /\t/ ;
+#	my ($lemma, $POS, $trans, $trans2, $trans3) = split /\t/ ;
+	print STDOUT "   <e>\n";
 	print STDOUT "      <lg>\n";
-	print STDOUT "         <l pos=\"$POS\">$D$H</l>\n";
+	print STDOUT "         <l pos=\"$POS\">$lemma</l>\n";
+#	print STDOUT "         <l pos=\"$POS\" type=\"Prop\">$lemma</l>\n";
 	print STDOUT "      </lg>\n";
 	print STDOUT "      <mg>\n";
-	print STDOUT "         <tg>\n";
-	print STDOUT "            <t pos=\"$POS\">$C$G</t>\n";
-	print STDOUT "            <t pos=\"$POS\">$E</t>\n";
-	print STDOUT "            <xg>\n";
-	print STDOUT "              <x>$F</x>\n";
-	print STDOUT "              <xt></xt>\n";
-	print STDOUT "            </xg>\n";
+	print STDOUT "         <tg xml:lang=\"smn\">\n";
+	print STDOUT "            <t pos=\"$POS\">$trans</t>\n";
+#	print STDOUT "            <xg>\n";
+#	print STDOUT "              <x></x>\n";
+#	print STDOUT "              <xt></xt>\n";
+#	print STDOUT "            </xg>\n";
 	print STDOUT "         </tg>\n";
 	print STDOUT "      </mg>\n";
 	print STDOUT "   </e>\n";
 }
+
+
+#{
+#	chomp ;
+##	my ($POS, $A, $B, $C, $D, $E, $F, $G, $H, $I, $J, $K) = split /\t/ ;
+#	print STDOUT "   <e src=\"MO\">\n";
+#	print STDOUT "      <lg>\n";
+#	print STDOUT "         <l pos=\"$POS\">$D$H</l>\n";
+#	print STDOUT "      </lg>\n";
+#	print STDOUT "      <mg>\n";
+#	print STDOUT "         <tg>\n";
+#	print STDOUT "            <t pos=\"$POS\">$C$G</t>\n";
+#	print STDOUT "            <t pos=\"$POS\">$E</t>\n";
+#	print STDOUT "            <xg>\n";
+#	print STDOUT "              <x>$F</x>\n";
+#	print STDOUT "              <xt></xt>\n";
+#	print STDOUT "            </xg>\n";
+#	print STDOUT "         </tg>\n";
+#	print STDOUT "      </mg>\n";
+#	print STDOUT "   </e>\n";
+#}
 
 
 
@@ -34,7 +59,7 @@ while (<>)
 print STDOUT "</r>\n";
 
 # A     1	    SUOMI
-# B     2	    SÄMIKIELÂ UUCÂMSÄÄNI
+# B     2	    SÄMIKIELÂ UUCCÂMSÄÄNI
 # C     3	    SAAMEN_HAK
 # D     4	    SUOMEN_HAK
 # E     5	    SAAMEN_RIN
